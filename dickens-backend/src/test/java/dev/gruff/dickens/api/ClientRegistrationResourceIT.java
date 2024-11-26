9fc45d410bcecfdbdab3dd8002bd67d946dc5580
Steve Poole <gruff.wizard@yahoo.com>
2024-11-26 11:15:33 +0000

    first commit

---
+++ dickens-backend/src/test/java/dev/gruff/dickens/api/ClientRegistrationResourceIT.java
@@ -0,0 +1,8 @@
+package dev.gruff.dickens.api;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class ClientRegistrationResourceIT extends ClientRegistrationResourceTest {
+    // Execute the same tests but in packaged mode.
+}
